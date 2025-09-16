115790e920e92190d7cc64390f5052f13b0b6e50
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-09-16 20:43:07 -0300

    commit

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -34,4 +34,27 @@ func main() {
 
 	array2[1] = "Posição Alterada"
 	fmt.Println(slice2)
+
+	//Arrays internos
+	fmt.Println("----------------")
+	slice3 := make([]float32, 10, 11)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice))
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 10)
+	slice4 = append(slice4, 10)
+
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
