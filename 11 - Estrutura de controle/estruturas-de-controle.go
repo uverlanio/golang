a2f0f495d366e03d83bdc2ee1b2003e6b60111bc
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-09-19 18:56:17 -0300

    commit

---
+++ 11 - Estrutura de controle/estruturas-de-controle.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Estruturas de controle")
+
+	numero := 10
+
+	if numero >= 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor ou igual a 15")
+	}
+
+	if outroNumero := numero; outroNumero > 0 {
+		fmt.Println("outroNumero é maior que zero")
+	} else if numero > -10 {
+		fmt.Println("outroNumero é menor que -10")
+	} else {
+		fmt.Println("Ente 0 e -10")
+	}
+}
