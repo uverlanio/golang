3a4be510b66f38198ef8773fcfe139652eaa33dc
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-09-22 18:57:48 -0300

    commit

---
+++ 13 - Loops/loops.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	/*i := 0
+
+	for i < 10 {
+		i++
+		fmt.Println("Incrementando i ")
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(i)*/
+
+	/*for j := 0; j < 10; j += 2 {
+		fmt.Println("Incrementando j", j)
+		time.Sleep(time.Second)
+	}*/
+
+	/*nomes := [3]string{"João", "Davi", "Lucas"}
+	for _, nome := range nomes {
+		fmt.Println(nome)
+	}*/
+
+	/*for indice, letra := range "PALAVRA" {
+		fmt.Println(indice, string(letra))
+	}*/
+
+	/*usuario := map[string]string{
+		"nome":       "Leonardo",
+		"sobrenome ": "Silva",
+	}
+
+	for chave, valor := range usuario {
+		fmt.Println(chave, valor)
+	}*/
+}
